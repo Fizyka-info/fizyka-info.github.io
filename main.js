7a400f175cfb0921ac493aa0935c9545e47381ff
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2021-06-11 10:12:38 +0200

    ???

--- main.js
+++ main.js
@@ -19,7 +19,28 @@ $(form.ajax).on('submit', function(){
      console.log(response);
    }
     });
-    
+    const express = require('express')
+const router = express.Router()
+    router.post('/user', (req, res) => {
+        console.log("Trying to create a new user...")
+        console.log("How do we get the form data???")
+      
+        console.log("First name: " + req.body.create_first_name)
+        const firstName = req.body.create_first_name
+        const lastName = req.body.create_last_name
+      
+        const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+        getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+          if (err) {
+            console.log("Failed to insert new user: " + err)
+            res.sendStatus(500)
+            return
+          }
+      
+          console.log("Inserted a new user with id: ", results.insertId);
+          res.end()
+        })
+      })
    
    return false;
    });
