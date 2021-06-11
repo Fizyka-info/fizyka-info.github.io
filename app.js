d7c37142aa16f7a230c19e4e7bd8e3000420a8e9
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2021-06-11 04:23:33 +0200

    change again

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("Hello from ROOOOOT")
 })
 
-const PORT = process.env.PORT || 21860
+const PORT = process.env.PORT || 5000
 // localhost:PORT
 app.listen(PORT, () => {
   console.log("Server is up and listening on: " + PORT)
