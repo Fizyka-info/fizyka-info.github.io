ddc005b5c171d434618c7fe7ba0dbea56dadaca7
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2021-06-11 19:59:14 +0200

    next question

---
+++ routes/user2.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const mysql = require('mysql')
+const router = express.Router()
+
+
+router.get('/messages', (req, res) => {
+  res.end()
+})
+
+router.get("/user2", (req, res) => {
+    const connection = getConnection()
+    const queryString = "SELECT * FROM users2"
+    connection.query(queryString, (err, rows, fields) => {
+      if (err) {
+        res.sendStatus(500)
+        return
+      }
+      res.json(rows)
+    })
+  })
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'be392d3481f8c6',
+    password: '37efc915',
+    database: 'heroku_576e0dbd383ebc6'
+})
+
+function getConnection() {
+    return pool
+}
+
+router.post('/user2', (req, res) => {
+
+ 
+    const firstQuestion = req.body.Q11
+    const secondQuestion = req.body.Q22
+    const thirdQuestion = req.body.Q33
+
+  
+    const queryString = "INSERT INTO users2 (first_question, second_question, third_question, fourth_question, fifth_question) VALUES (?, ?, ?)"
+    getConnection().query(queryString, [firstQuestion, secondQuestion, thirdQuestion], (err, results, fields) => {
+      if (err) {
+        res.sendStatus(500)
+        return
+      }
+      res.redirect("/atomowa3.html");
+      res.end()
+    })
+  })
+  
+router.get('/user2/:id', (req, res) => {
+
+    const connection = getConnection()
+
+    const userId = req.params.id
+    const queryString = "SELECT * FROM users2 WHERE id = ?"
+   connection.query(queryString, [userId], (err, rows, fields) => {
+        if (err) {
+        res.sendStatus(500)
+        return
+        }
+
+        const users2 = rows.map((row) => {
+        return {firstQuestion: row.first_question, secondQuestion: row.second_question, thirdQuestion: row.third_question}
+        })
+
+        res.json(users2)
+    })
+})
+
+module.exports = router
